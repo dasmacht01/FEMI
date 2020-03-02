283e483326cb34c49c00fa3b4f2539d1b7f519bd
Liu Renhang <61110365+Samillynn@users.noreply.github.com>
2020-03-02 17:30:39 +0800

    Complete select_tea and add comments to lrh.py

--- lrh.py
+++ lrh.py
@@ -1,33 +1,38 @@
 from share import *
-import telegram
-# TODO:
-#  select_tea
-#  select_sugar
-#  select_ice
-#  select_toppings
 
-# def select_tea(update, context):
-#    f
 
-def select_sugar(update: telegram.Update, context):
+def select_tea(update, context):
+    """ show teas of the catagory user chose """
+    subcategory = product_dict[update.callback_query.data]
+    buttons = [[(tea, tea)] for tea in categories[subcategory]]
+    update.callback_query.edit_message_text('Choose what you want to drink:',
+                                            reply_markup=inline_keyboard(buttons))
 
-    # TODO:
-    #  ask callback_data form of last query
-    #  discuss about global variable current_item
-    current_item.name = update.callback_query.data
 
-    text = f'You have chosen {current_item.name}, please customize your product.'
+def select_sugar(update, context):
+    """ function asking for sugar level """
+
+    # create current_item object due to the tea the user chose
+    current_item = Tea(name=update.callback_query.data)
+
+    # send message for user to choose sugar level
+    text = f'You have chosen {current_item.name}, please customize your product.\n' \
+           'Choose sugar level:'
     buttons = [[('No Sugar', 'sugar-0')],
                [('25%', 'sugar-25')],
                [('50%', 'sugar-50')],
                [('75%', 'sugar-75')],
                [('Full Sugar', 'sugar-100')]]
-    update.callback_query.edit_message_text(text)
-    update.message.reply_message('Choose sugar level:',
-                                 reply_markup=inline_keyboard(buttons))
+    update.callback_query.edit_message_text(text, reply_markup=inline_keyboard(buttons))
+
 
 def select_ice(update, context):
+    """ function to ask for ice level """
+
+    # record sugar level to current_item object
     current_item.suagr = int(update.callback_query.data[6:])
+
+    # send message for user to choose ice level
     buttons = [[('Hot', 'ice-hot')],
                [('No Ice', 'ice-no')],
                [('Less Ice', 'ice-less')],
@@ -38,7 +43,12 @@ def select_ice(update, context):
 
 
 def select_toppings(update, context):
+    """ function to ask for toppings"""
+
+    # record ice level to current_item object
     current_item.ice = update.callback_query.data[4:]
+
+    # send message for user to choose toppings
     buttons = [[('Pearl', 'pearl'), ('Red Bean', 'red-bean')],
                [('Coconut', 'coconut'), ('Milk', 'milk')],
                [('That\'s all', 'rh-Done')]]
