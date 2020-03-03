c991ca0afcb581a74f29829bcc2b9d8113227959
Zhengnan Sun <56228007+dasmacht01@users.noreply.github.com>
2020-03-03 22:41:56 +0800

    Add files via upload

--- editable_order.py
+++ editable_order.py
@@ -1,18 +1,20 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from share import inline_keyboard, Tea
 
+from share import price_dict, Tea
+import random as r
+
 
 def reply(update, context):
     text = 'Here are your orders.\n' 'Tap any selection if you want to cancel it.\n'
     buttons = []
     for i in range(len(cart)):
         buttons.append([(f'{i + 1}. '+str(cart[i]), str(i))])
-    print(buttons)
     update.message.reply_text(text, reply_markup=inline_keyboard(buttons))
 
 
 def main():
-    updater = Updater("TOKEN", use_context=True)
+    updater = Updater("1121470779:AAG5F0CaZAcU9oQW34lXVzTrKMgZGoyDtNg", use_context=True)
     dp = updater.dispatcher
     dp.add_handler(MessageHandler(Filters.text, reply))
     updater.start_polling()
@@ -20,8 +22,10 @@ def main():
 
 
 if __name__ == '__main__': # need create test set
-    tea1 = Tea('yolo')
-    tea2 = Tea('yes')
-    tea3 = Tea('haha')
-    cart = [tea1, tea2, tea3]
+    selections = r.choices(list(price_dict.keys()), k=4)
+    toppings_list = ['Pearl', 'Red Bean', 'Coconut', 'Milk']
+    ice_list = ['hot', 'no', 'less', 'regular', 'more']
+    cart = [Tea(tea, r.randint(0, 5) * 25, r.choice(ice_list), r.choices(toppings_list, k=r.randint(0, 3))) for tea in
+            selections]
+    print(cart)
     main()
