79b4ce85598deff5a32095a84892f1a7a8521c18
Zhengnan Sun <56228007+dasmacht01@users.noreply.github.com>
2020-03-06 12:08:06 +0800

    Add files via upload

--- editable_order.py
+++ editable_order.py
@@ -1,41 +1,51 @@
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
 from share import inline_keyboard, Tea, price_dict
-from test import delete
 import random as r
 import time
+import pprint # ??
+
+text = 'Here are your orders.\n' 'Tap any selection if you want to cancel it.\n'
 
 
 def start(update, context):
-    buttons = []
-    for i in range(len(cart)):
-        buttons.append([(f'{i + 1}. '+str(cart[i]), str(i))])
-    buttons.append([('set order', str(i + 1))]) # will raise IndexError
-    update.message.reply_text(text, reply_markup=inline_keyboard(buttons))
+    print(update)
+    chat_id = update.message.chat_id
+    update.message.reply_text(text, reply_markup=inline_keyboard(make_order_keyboard(cart)))
 
 
 def delete(update, context):
-    choice = update.callback_query.data
-    if cart:
-        try:
-            # why I have to define text here again??
-            text = 'You have deleted ' + str(cart.pop(int(choice)).name) + '\n' + \
-                   'Here are your orders.\n' + 'Tap any selection if you want to cancel it.\n'
-
-            buttons = []
-            for i in range(len(cart)):
-                buttons.append([(f'{i + 1}. ' + str(cart[i]), str(i))])
-            buttons.append([('set order', str(i + 1))])
-
-            update.callback_query.edit_message_text(text)
+    #print(update)
+    global text
+    choice = int(update.callback_query.data)
+    try:
+        # why I have to define text here again??
+        text = 'You have deleted ' + str(cart.pop(choice).name) + '\n' + text
+        #text = 'You have deleted ' + str(cart.pop(choice).name) + '\n' + \
+         #      'Here are your orders.\n' + 'Tap any selection if you want to cancel it.\n'
+        #update.callback_query.reply_action('typing')
+        time.sleep(1)
+        update.callback_query.edit_message_text(text)
+        if cart:
+            #update.callback_query.reply_action('typing')
             time.sleep(1)
-            update.callback_query.edit_message_reply_markup(reply_markup=inline_keyboard(buttons))
-        except IndexError:
-            print('go to total_and_address page')
-            # go to total_and_address page
-    else:
-        update.callback_query.edit_message_text('Your cart is emtpy now!\nTap "back" to return to home page.')
-        time.sleep(2)
-        update.callback_query.edit_message_reply_markup(reply_markup=inline_keyboard([[('back', 'back')]]))
+            update.callback_query.edit_message_reply_markup(reply_markup=inline_keyboard(make_order_keyboard(cart)))
+        else:
+            update.callback_query.edit_message_text('Your cart is emtpy now!\nTap "back" to return to home page.')
+            time.sleep(1)
+            update.callback_query.edit_message_reply_markup(reply_markup=inline_keyboard([[('back', 'back')]]))
+    except IndexError:
+        # go to total_and_address page
+        update.callback_query.reply_text('got to total_and_address page')
+
+
+
+def make_order_keyboard(list_): # return a reply markup
+    buttons = []
+    for i in range(len(list_)):
+        buttons.append([(f'{i + 1}. ' + str(list_[i]), str(i))])
+    buttons.append([('set order', str(i + 1))]) # will raise IndexError
+    return buttons
+
 
 def main():
     updater = Updater("1121470779:AAG5F0CaZAcU9oQW34lXVzTrKMgZGoyDtNg", use_context=True)
@@ -48,12 +58,10 @@ def main():
 
 
 if __name__ == '__main__': # need create test set
-    '''global text
-    text = 'Here are your orders.\n' 'Tap any selection if you want to cancel it.\n''''
     selections = r.choices(list(price_dict.keys()), k=4)
     toppings_list = ['Pearl', 'Red Bean', 'Coconut', 'Milk']
     ice_list = ['hot', 'no', 'less', 'regular', 'more']
-    cart = [Tea(tea, r.randint(0, 4) * 25, r.choice(ice_list), r.choices(toppings_list, k=r.randint(0, 3))) for tea in
-            selections]
+    cart = [Tea(tea, r.randint(0, 4) * 25, r.choice(ice_list),
+                r.choices(toppings_list, k=r.randint(0, 3))) for tea in selections]
     print(cart)
     main()
